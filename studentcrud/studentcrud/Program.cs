50f9393f567ca9dc9ba63f907d66b3338dd5509c
sachinwankhede123 <98028062+sachinwankhede123@users.noreply.github.com>
2024-04-18 22:12:09 +0530

    Add files via upload

---
+++ studentcrud/studentcrud/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentcrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection sqlConnection;
+            String connectionString = @"Data Source=SACHIN-PC\SQLEXPRESS;Initial Catalog=studentCRUD;Integrated Security=True;Encrypt=False";
+           SqlConnection
+          sqlconnection = new SqlConnection(connectionString);
+            sqlconnection.Open();
+            Console.WriteLine("Name:");
+            string name=Console.ReadLine();
+
+
+            Console.WriteLine("Gender:");
+            string gender = Console.ReadLine();
+
+            Console.WriteLine("Email:");
+            string email = Console.ReadLine();
+
+            string insertQuery="insert into record(name,gender,email)values('"+name+"', '"+gender+"', '"+email+"')";
+
+            SqlCommand insertCommand = new SqlCommand(insertQuery,sqlconnection);
+            insertCommand.ExecuteNonQuery();
+            Console.WriteLine("Data inserted");
+            sqlconnection.Close();
+
+            Console.ReadKey();
+                }
+    }
+}
